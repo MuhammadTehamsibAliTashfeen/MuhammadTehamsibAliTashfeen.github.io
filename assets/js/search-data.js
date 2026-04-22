e453e493cb5449e7f5cbf8b349b0e7408ec80ea5
MuhammadTehamsibAliTashfeen <88139073+MuhammadTehamsibAliTashfeen@users.noreply.github.com>
2026-04-22 03:56:25 +0000

    Deploying to gh-pages from @ MuhammadTehamsibAliTashfeen/MuhammadTehamsibAliTashfeen.github.io@3119e90bc13b143a820a4f2f559f6b366e461e6a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "PhD Candidate in Electrical Engineering &amp; Computer Science · Wichita State University",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
